46070a458d8e0f75195e7cfc9609019e478e6ccc
noyonalways <noyonrahman2003@gmail.com>
2023-08-22 22:13:57 +0600

    new update from noyonalways

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-const app = require('./app');
-const dotenv = require('dotenv').config();
+const app = require("./app");
+const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
-const colors = require('colors');
-const mongoose = require('mongoose');
+const colors = require("colors");
+const mongoose = require("mongoose");
 
-// Database URL
+// mongodb atlas cloude db url
+const dbURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.lpsfjtz.mongodb.net/?retryWrites=true&w=majority`;
 
-const dbURL =  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.lpsfjtz.mongodb.net/?retryWrites=true&w=majority`; // mongodb atlas cloude db url
-
-const dbLocalURL = `mongodb://localhost:27017/${process.env.APP_NAME}`
+// mongodb compass url
+const dbLocalURL = `mongodb://localhost:27017/${process.env.APP_NAME}`;
 
 // Connect to the database asynchronously
-mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Database connection successfull ✔'.blue.bold);
-  })
-  .catch((error) => {
-    console.error('Error connecting to the database:', error);
-  });
-
-
+mongoose
+	.connect(dbURL, {
+		// useNewUrlParser: true,
+		// useUnifiedTopology: true,
+		// serverSelectionTimeoutMS: 5000,
+		// autoIndex: false, // Don't build indexes
+		// maxPoolSize: 10, // Maintain up to 10 socket connections
+		// serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+		// socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+		family: 4, // Use IPv4, skip trying IPv6
+	})
+	.then(() => {
+		console.log("Database connection successfull ✔".blue.bold);
+	})
+	.catch((error) => {
+		console.error("Error connecting to the database:", error);
+	});
 
 app.listen(port, () => {
-    console.log(`App is running on ${port}`.yellow.bold, `http://localhost:${port}`);
+	console.log(
+		`App is running on ${port}`.yellow.bold,
+		`http://localhost:${port}`
+	);
 });
